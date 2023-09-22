d03af16bf47f238becbaed1d8e65eaaff281505a
korolka <korolka212@gmail.com>
2023-09-22 15:02:17 +0300

    first commit

---
+++ AspUi5/Controllers/Ex1Controller.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspUi5.Controllers
+{
+    public class Ex1Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ AspUi5/Controllers/ExternalExAndEx2Controller.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspUi5.Controllers
+{
+    public class ExternalExAndEx2Controller : Controller
+    {
+        public IActionResult MainPage()
+        {
+            return View();
+        }
+
+        public IActionResult Page1()
+        {
+            return View();
+        }
+
+        public IActionResult Page2()
+        {
+            return View();
+        }
+
+
+        public IActionResult Menu1()
+        {
+            return View();
+        }
+
+        public IActionResult Menu2()
+        {
+            return View();
+        }
+
+        public IActionResult Menu3()
+        {
+            return View();
+        }
+    }
+}
